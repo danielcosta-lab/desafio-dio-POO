d11dc460b4a485635bf0a335c1adf65abc237c03
danielcosta-lab <daniel.costa0494@gmail.com>
2022-09-29 10:59:49 -0300

    Projeto POO

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,41 +1,17 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
 
-    private String titulo;
-    private String descricao;
     private int cargaHoraria;
+
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + cargaHoraria;
+    }
 
     public Curso () {
         
-    }
-
-      /**
-     * @return String return the titulo
-     */
-    public String getTitulo() {
-        return titulo;
-    }
-
-    /**
-     * @param titulo the titulo to set
-     */
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    /**
-     * @return String return the descricao
-     */
-    public String getDescricao() {
-        return descricao;
-    }
-
-    /**
-     * @param descricao the descricao to set
-     */
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 
     /**
@@ -60,4 +36,5 @@ public class Curso {
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
     }
+
 }
